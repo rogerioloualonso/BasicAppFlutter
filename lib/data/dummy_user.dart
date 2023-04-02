bed20d636b15e72aeacd58cee92910a19d16cb84
Rogério Alonso <rogeriolalonso@hotmail.com>
2023-04-02 16:12:41 -0300

    Diferenciando os dados

--- lib/data/dummy_user.dart
+++ lib/data/dummy_user.dart
@@ -9,14 +9,14 @@ const DUMMY_USERS = {
           'https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_960_720.png'),
   '2': User(
       id: '1',
-      name: 'Maria',
-      email: 'maria@alunos.com.br',
+      name: 'João',
+      email: 'João@alunos.com.br',
       avatarUrl:
-          'https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_960_720.png'),
+          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'),
   '3': User(
       id: '1',
-      name: 'Maria',
-      email: 'maria@alunos.com.br',
+      name: 'Zézinho',
+      email: 'Zézinho@alunos.com.br',
       avatarUrl:
-          'https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_960_720.png'),
+          'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png'),
 };
