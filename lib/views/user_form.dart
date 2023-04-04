f8996e2ae505d5b26ffbf37b7a1e1dfcf6f1ae73
Rogério Alonso <rogeriolalonso@hotmail.com>
2023-04-04 17:22:55 -0300

    Inserir usuário

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,17 +1,62 @@
+import 'package:exemplo/models/user.dart';
+import 'package:exemplo/provider/users.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Formulário de Usuários'), actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.save),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        )
-      ]),
-    );
+        appBar: AppBar(title: Text('Formulário de Usuários'), actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final isValid = _form.currentState!.validate();
+              if (isValid) {
+                _form.currentState?.save();
+                Provider.of<Users>(context, listen: false).put(
+                  User(
+                    id: _formData['id']!,
+                    name: _formData['name']!,
+                    email: _formData['email']!,
+                    avatarUrl: _formData['avatarUrl']!,
+                  ),
+                );
+                Navigator.of(context).pop();
+              }
+            },
+          )
+        ]),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Form(
+              key: _form,
+              child: Column(children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nome'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Nome inválido';
+                    }
+                    if (value.trim().length <= 3) {
+                      return 'Nme muito pequeno, no mínimo 3 letras.';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _formData['name'] = value!,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                  onSaved: (value) => _formData['email'] = value!,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Url do avatar'),
+                  onSaved: (value) => _formData['avatarUrl'] = value!,
+                ),
+              ])),
+        ));
   }
 }
